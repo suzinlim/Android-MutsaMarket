e048f4d28a06c288f5b481ff4429c39efc982193
Sengoo <lesley023@naver.com>
2023-10-26 12:45:45 +0900

    글 등록페이지 사진추가
    사진추가기능 제작, 권한 여부 확인 오류나서 일단 제외하고 제작

--- app/src/main/java/hansung/ac/mutsamarket/ui/make_content/MakeContentFragment.kt
+++ app/src/main/java/hansung/ac/mutsamarket/ui/make_content/MakeContentFragment.kt
@@ -1,42 +1,73 @@
 package hansung.ac.mutsamarket.ui.make_content
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import hansung.ac.mutsamarket.R
 import hansung.ac.mutsamarket.databinding.FragmentMakeContentBinding
+import kotlin.math.log
 
 class MakeContentFragment : Fragment() {
 
     private var _binding: FragmentMakeContentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    companion object {
+        private const val REQUEST_IMAGE_PICK = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val makeContentViewModel =
-            ViewModelProvider(this).get(MakeContentViewModel::class.java)
-
         _binding = FragmentMakeContentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        //val textView: TextView = binding.textMakeContent
-        //makeContentViewModel.text.observe(viewLifecycleOwner) {
-            //textView.text = it
-        //}
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val picButton = view.findViewById<Button>(R.id.pic_button)
+        picButton.setOnClickListener { //사진추가 이벤트
+            openGallery()
+        }
+    }
+
+
+    val REQ_GALLERY = 1
+    fun openGallery() { //갤러리열기
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, REQ_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri: Uri = data.data!!
+
+            // 선택한 이미지를 ImageView에 설정
+            binding.imageView.setImageURI(selectedImageUri)
+        }
     }
 }
