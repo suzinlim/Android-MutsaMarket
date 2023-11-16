306771e935d7a270421ac00738e4988eeea821da
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-16 15:51:35 +0900

    refactor: signup

--- app/src/main/java/hansung/ac/mutsamarket/SignupActivity.kt
+++ app/src/main/java/hansung/ac/mutsamarket/SignupActivity.kt
@@ -20,6 +20,8 @@ class SignupActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
     lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+
+    var isEmailChecked = false // 이메일 중복 확인 여부
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
@@ -30,24 +32,6 @@ class SignupActivity : AppCompatActivity() {
 
         // 데이터베이스 객체 초기화
         database = Firebase.database.reference
-
-        binding.signupButton.setOnClickListener {
-            val name = binding.userName.text.toString().trim()
-            val email = binding.userEmail.text.toString().trim()
-            val password = binding.password.text.toString().trim()
-            val checkPassword = binding.checkPassword.text.toString().trim()
-
-            if (password.length < 6 || password.length > 15 || !password.matches(Regex(".*[a-zA-Z].*"))) {
-                // 비밀번호 조건에 맞지 않을 경우
-                Toast.makeText(this, "비밀번호는 6~15자의 영문 조합이어야 합니다.", Toast.LENGTH_SHORT).show()
-            } else if (password != checkPassword) {
-                // 비밀번호와 확인 비밀번호가 일치하지 않을 경우
-                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
-            } else {
-                // 비밀번호 조건이 충족될 경우, 회원가입 시도
-                signUp(name, email, password)
-            }
-        }
 
         binding.checkButton.setOnClickListener {
             val verifiedEmail = binding.userEmail.text.toString().trim()
@@ -60,17 +44,46 @@ class SignupActivity : AppCompatActivity() {
                     if (dataSnapshot.exists()) {
                         // 이메일이 이미 존재하는 경우
                         Toast.makeText(this@SignupActivity, "이미 사용 중인 이메일입니다.", Toast.LENGTH_SHORT).show()
+                        binding.signupButton.isEnabled = false // 회원가입 버튼 비활성화
                     } else {
                         // 이메일이 존재하지 않는 경우
                         Toast.makeText(this@SignupActivity, "사용 가능한 이메일입니다.", Toast.LENGTH_SHORT).show()
+                        isEmailChecked = true // 이메일 중복 확인 성공
+                        binding.signupButton.isEnabled = true // 회원가입 버튼 활성화
                     }
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
                     // 쿼리 중 오류가 발생한 경우
                     Log.e("Firebase", "Error: ${databaseError.message}")
+                    binding.signupButton.isEnabled = false // 회원가입 버튼 비활성화
                 }
             })
+        }
+
+        binding.signupButton.setOnClickListener {
+            val name = binding.userName.text.toString().trim()
+            val email = binding.userEmail.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+            val checkPassword = binding.checkPassword.text.toString().trim()
+
+            // 비밀번호 조건에 맞지 않을 경우
+            if (password.length < 6 || password.length > 15 || !password.matches(Regex(".*[a-zA-Z].*"))) {
+                Toast.makeText(this, "비밀번호는 6~15자의 영문 조합이어야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // 회원가입 함수 호출하지 않음
+            }
+            // 비밀번호와 확인 비밀번호가 일치하지 않을 경우
+            if (password != checkPassword) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // 회원가입 함수 호출하지 않음
+            }
+            // 이메일 중복 확인을 하지 않은 경우
+            if (!isEmailChecked) {
+                Toast.makeText(this, "이메일 중복 확인을 해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // 회원가입 함수 호출하지 않음
+            }
+            // 비밀번호 조건이 충족되고 이메일 중복 검사를 한 경우, 회원가입 시도
+            signUp(name, email, password)
         }
     }
 
@@ -80,12 +93,13 @@ class SignupActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // 회원가입 성공할 경우
-                    Log.d("SignUp","createUserWithEmail:success")
+                    Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                     val intent: Intent = Intent(this@SignupActivity, LoginActivity::class.java)
                     startActivity(intent)
                     addUserToDatabase(name, email, auth.currentUser?.uid!!)
                 } else {
                     // 회원가입 실패할 경우
+                    Toast.makeText(this, "회원가입에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
                     Log.w("SignUp", "Error: ${task.exception}")
                 }
             }
