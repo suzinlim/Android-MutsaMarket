48dbee1642784e75031e1e344394fe66fd3702d7
Sengoo <lesley023@naver.com>
2023-11-16 19:38:57 +0900

    글 등록 firebase 연동

--- app/src/main/java/hansung/ac/mutsamarket/ui/make_content/MakeContentFragment.kt
+++ app/src/main/java/hansung/ac/mutsamarket/ui/make_content/MakeContentFragment.kt
@@ -1,10 +1,7 @@
 package hansung.ac.mutsamarket.ui.make_content
 
-import android.Manifest
 import android.app.Activity
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,21 +10,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.widget.EditText
+import android.widget.MultiAutoCompleteTextView
+import android.widget.Switch
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import hansung.ac.mutsamarket.R
 import hansung.ac.mutsamarket.databinding.FragmentMakeContentBinding
-import kotlin.math.log
-import androidx.navigation.fragment.findNavController
+import hansung.ac.mutsamarket.vo.Post
 
 class MakeContentFragment : Fragment() {
+
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val itemsCollectionRef = db.collection("items")
+    private lateinit var selectedImageUri: Uri
 
     private var _binding: FragmentMakeContentBinding? = null
     private val binding get() = _binding!!
 
     companion object {
-        private const val REQUEST_IMAGE_PICK = 1
+        private const val REQ_GALLERY = 1
+        private const val TAG = "MakeContentFragment"
     }
 
     override fun onCreateView(
@@ -44,25 +50,47 @@ class MakeContentFragment : Fragment() {
         _binding = null
     }
 
-
-    //사진추가하는 코드+페이지 이동 코드
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val writeButton=view.findViewById<Button>(R.id.write_button) //등록하기
-        val picButton = view.findViewById<Button>(R.id.pic_button) //사진추가
-        picButton.setOnClickListener { //사진추가 이벤트
+
+        val writeButton = view.findViewById<Button>(R.id.write_button)
+        val picButton = view.findViewById<Button>(R.id.pic_button)
+        val titleEditText = view.findViewById<EditText>(R.id.edit_name)
+        val priceEditText = view.findViewById<EditText>(R.id.edit_price)
+        val contentEditText = view.findViewById<MultiAutoCompleteTextView>(R.id.edit_content)
+        val sellSwitch = view.findViewById<Switch>(R.id.sell_switch)
+
+        picButton.setOnClickListener {
             openGallery()
         }
 
-        writeButton.setOnClickListener{//등록하기 이벤트
-            val navController = findNavController()
-            navController.navigate(R.id.action_navigation_make_content_to_navigation_home)
-       }
+        writeButton.setOnClickListener {
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser != null) {
+                val writer = currentUser.displayName ?: "Anonymous"
+
+                val title = titleEditText.text.toString()
+                val price = priceEditText.text.toString()
+                val content = contentEditText.text.toString()
+                val isSale = sellSwitch.isChecked
+
+                val post = Post(selectedImageUri.toString(), title, price, writer, content, isSale)
+
+                itemsCollectionRef.add(post)
+                    .addOnSuccessListener { documentReference ->
+                        val navController = findNavController()
+                        navController.navigate(R.id.action_navigation_make_content_to_navigation_home)
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(requireContext(), "글 작성에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                Toast.makeText(requireContext(), "사용자가 로그인되어 있지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
-
-    val REQ_GALLERY = 1
-    fun openGallery() { //갤러리열기
+    private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
         startActivityForResult(intent, REQ_GALLERY)
@@ -72,13 +100,8 @@ class MakeContentFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == REQ_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
-            val selectedImageUri: Uri = data.data!!
-
-            // 선택한 이미지를 ImageView에 설정
+            selectedImageUri = data.data!!
             binding.imageView.setImageURI(selectedImageUri)
         }
     }
-
-
 }
-
