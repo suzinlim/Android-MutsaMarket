2912c6f58bb13e6da5c07321a69a8f03ee775c79
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-16 16:21:14 +0900

    feat: splash 추가

--- app/src/main/java/hansung/ac/mutsamarket/SignupActivity.kt
+++ app/src/main/java/hansung/ac/mutsamarket/SignupActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import hansung.ac.mutsamarket.databinding.ActivitySignupBinding
+import hansung.ac.mutsamarket.vo.User
 
 class SignupActivity : AppCompatActivity() {
 
