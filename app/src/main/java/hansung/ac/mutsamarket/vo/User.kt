9f804080069b228f9098a9f70e937d17a1606435
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-11-21 02:22:21 +0900

    feat: 생년월일 정보 추가

--- app/src/main/java/hansung/ac/mutsamarket/vo/User.kt
+++ app/src/main/java/hansung/ac/mutsamarket/vo/User.kt
@@ -2,8 +2,9 @@ package hansung.ac.mutsamarket.vo
 
 data class User(
     var name: String,
+    var birth: String,
     var email: String,
     var uid: String
 ) {
-    constructor(): this("", "", "")
+    constructor(): this("", "", "", "")
 }
