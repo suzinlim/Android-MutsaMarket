4a3c4d4ee0b17d9724e83a27a8bb124cba65c65c
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-11-26 22:17:34 +0900

    메세지 구분

--- app/src/main/java/hansung/ac/mutsamarket/ui/chatting/MessageAdapter.kt
+++ app/src/main/java/hansung/ac/mutsamarket/ui/chatting/MessageAdapter.kt
@@ -1,40 +1,80 @@
-//MessageAdapter.kt
+// MessageAdapter.kt
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import hansung.ac.mutsamarket.R
+import androidx.viewbinding.ViewBinding
+import hansung.ac.mutsamarket.databinding.ItemMyMessageBinding
+import hansung.ac.mutsamarket.databinding.ItemOtherMessageBinding
 import hansung.ac.mutsamarket.vo.Message
 
-class MessageAdapter(private var messages: List<Message>) :
-    RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
-
-    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val messageTextView: TextView = itemView.findViewById(R.id.messageTextView)
-    }
+class MessageAdapter(
+    private var messages: List<Message>,
+    private val currentUserId: String
+) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
     fun updateData(newMessages: List<Message>) {
         messages = newMessages
         notifyDataSetChanged()
     }
 
-    fun getMessages(): List<Message> {
-        return messages
-    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_message, parent, false)
-        return MessageViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            VIEW_TYPE_MY_MESSAGE -> {
+                val binding = ItemMyMessageBinding.inflate(inflater, parent, false)
+                MyMessageViewHolder(binding)
+            }
+            VIEW_TYPE_OTHER_MESSAGE -> {
+                val binding = ItemOtherMessageBinding.inflate(inflater, parent, false)
+                OtherMessageViewHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
-        holder.messageTextView.text = message.content
+        when (holder) {
+            is MyMessageViewHolder -> holder.bindMyMessage(message)
+            is OtherMessageViewHolder -> holder.bindOtherMessage(message)
+        }
     }
 
-    override fun getItemCount(): Int {
-        return messages.size
+    override fun getItemCount(): Int = messages.size
+
+    override fun getItemViewType(position: Int): Int {
+        val message = messages[position]
+        return if (message.senderId == currentUserId) {
+            VIEW_TYPE_MY_MESSAGE
+        } else {
+            VIEW_TYPE_OTHER_MESSAGE
+        }
     }
 
+    abstract class MessageViewHolder(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    inner class MyMessageViewHolder(private val binding: ItemMyMessageBinding) :
+        MessageViewHolder(binding) {
+        fun bindMyMessage(message: Message) {
+            binding.textMessageContentMy.text = message.content
+            binding.textMessageContentOther.visibility = View.GONE
+            // 추가적인 내 메시지 레이아웃 설정 작업 수행
+        }
+    }
+
+    inner class OtherMessageViewHolder(private val binding: ItemOtherMessageBinding) :
+        MessageViewHolder(binding) {
+        fun bindOtherMessage(message: Message) {
+            binding.textMessageContentOther.text = message.content
+            binding.textMessageContentMy.visibility = View.GONE
+            // 추가적인 상대방 메시지 레이아웃 설정 작업 수행
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_MY_MESSAGE = 1
+        private const val VIEW_TYPE_OTHER_MESSAGE = 2
+    }
 }
