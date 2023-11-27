58c5b05b1ac34ccb186a804e3957d14f2a756a8a
이현승 <77336664+gesal03@users.noreply.github.com>
2023-11-27 17:03:10 +0900

    end

--- app/src/main/java/hansung/ac/mutsamarket/MainActivity.kt
+++ app/src/main/java/hansung/ac/mutsamarket/MainActivity.kt
@@ -24,13 +24,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_chatting, R.id.navigation_make_content, R.id.navigation_my_page,
-                R.id.navigation_post_detail
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_chatting, R.id.navigation_make_content, R.id.navigation_my_page,
+//                R.id.navigation_post_detail
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
